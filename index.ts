4882d7967823736e9f6eb9931fddf22c8994bfdd
Antti Nevala <antti.nevala@outlook.com>
2020-06-06 19:35:31 +0300

    Trim refs/heads/ from base branch name

--- index.ts
+++ index.ts
@@ -37,25 +37,25 @@ function updateMergeBranch(name:string, sha:string) {
     });
 }
 
-async function doesBranchExist(name:string):Promise<boolean> {
+async function isExistingBranch(name:string):Promise<boolean> {
     const matchingResults = await octokit.git.listMatchingRefs({
         ...context.repo,
         ref: 'refs/heads/' + name
     });
 
-    console.log(matchingResults, 'Matches for existing branch');
+    console.log('Matches for existing branch', matchingResults);
 
     return matchingResults.data.length > 0;
 }
 
-async function doesPullRequestExist(headBranchName:string) {
+async function isExistingPullRequest(headBranchName:string) {
     const matchingResults = await octokit.pulls.list({
         ...context.repo,
         state: 'open',
         head: headBranchName, // TODO Don't know if we need organization here
     });
 
-    console.log(matchingResults, 'Matches for existing PR');
+    console.log('Matches for existing PR', matchingResults);
 
     return matchingResults.data.length > 0;
 }
@@ -69,7 +69,8 @@ async function run() {
     octokit = new github.GitHub(token);
     context = github.context;
 
-    const fromBranch = context.ref;
+    // Get name of triggering branch witout refs/heads/
+    const fromBranch = context.ref.slice(10);
 
     // TODO Validate that changes happened in from brach. Either here or in workflow configuration if possible
     // TODO Use wildcards in validating branch
@@ -79,14 +80,14 @@ async function run() {
     // TODO Merge branch if auto merge is on
 
     const inConflict = false;
-    const isExistingMergeBranch = await doesBranchExist(mergeBranchName);
+    const isExistingMergeBranch = await isExistingBranch(mergeBranchName);
     var isExistingPR = false;
 
     // Create merge branch if conflict or update existing branch
     if (isExistingMergeBranch) {
         // TODO Update reference with added commits
         updateMergeBranch(mergeBranchName, context.sha);
-        isExistingPR = await doesPullRequestExist(mergeBranchName);
+        isExistingPR = await isExistingPullRequest(mergeBranchName);
     } else {
         createBranch(mergeBranchName, context.sha);
     }
